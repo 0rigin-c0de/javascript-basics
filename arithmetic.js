58d21e2ca7aa44cca1cb5a4dde47a3cfc442d987
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-15 22:53:45 +0530

    initial mathmatics in javaScript

---
+++ arithmetic.js
@@ -0,0 +1,11 @@
+// Arithmetic and math functions in javascript
+
+
+var x= 3;
+var y =1.1;
+console.log(x + y);
+console.log(x * y);
+console.log((x+y)*(x-y));
+var z = 9;
+console.log(Math.sqrt(z));
+console.log(Math.abs(y/x));
