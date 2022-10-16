f7e2c6ee58b0f81904c3ab1a47d7b873c1c5567e
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-16 11:39:23 +0530

    how to call function in javascript code

---
+++ practiceJavaScript/practice.js
@@ -0,0 +1,16 @@
+// this piece of code shows how to call a function;
+function Person(first, middle, last) {
+    this.first = first;
+    this.middle = middle;
+    this.last = last;
+    this.initials = initials;
+ }
+
+ function initials() {
+    return this.first[0] + this.middle[0] + this.last[0];
+ }
+ var aPerson = new Person("Sunil","kumar","Bhardwaj");
+ console.log("First name: " + aPerson.first);
+ console.log("Middle name: " + aPerson.middle);
+ console.log("Last name: " + aPerson.last);
+ console.log("Initials: " + aPerson.initials());
