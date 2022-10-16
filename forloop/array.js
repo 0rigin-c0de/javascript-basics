eecc28d4f6773145cfe31ecf016649869f0db45a
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-16 22:48:36 +0530

    initialize for loop using array in javascript code

---
+++ forloop/array.js
@@ -0,0 +1,7 @@
+// this code shows how to initialize for loop using array;
+var numbers =[3, 7, 12, 22, 100];
+var sum = 0;
+for(var i = 0; i < numbers.length;++i){
+    sum+=numbers[i];
+}
+console.log(sum);
