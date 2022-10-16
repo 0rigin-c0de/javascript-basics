84e7ff7be2a918c66173a7ec0d30d33d7ace1465
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-16 22:57:04 +0530

    initialize how to call a function in javascript

---
+++ functions/factorial.js
@@ -0,0 +1,13 @@
+
+//this piece of code demonstrates how value-returning functions are defined and called in Javascript
+
+function factorial(number) {
+    var product = 1;
+    for (var i = number; i >= 1; --i) {
+    product *= i;
+    }
+    return product;
+   }
+   console.log(factorial(4)); // displays 24
+   console.log(factorial(5)); // displays 120
+   console.log(factorial(10)); // displays 3628800
