9a34cb18880ccdaae4df95cc740da00e3e5ca0e2
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-16 22:16:03 +0530

    intial while loop in javascript code

---
+++ practiceJavaScript/whileloop.js
@@ -0,0 +1,10 @@
+// this piece of code demonstrates how while loop works in javascript code;
+
+
+var number = 1;
+var sum =0;
+while(number<11){
+    sum+=number;
+    ++number;
+}
+console.log(sum); // display 55
