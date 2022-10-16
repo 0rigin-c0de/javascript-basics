8cbb1e93820647204180e2810e037cfcb243b920
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-16 11:45:24 +0530

    initial simple if statement in javascript code

---
+++ practiceJavaScript/simpleif.js
@@ -0,0 +1,9 @@
+// this example shows how to write a simple if statement in JavaScript code;
+var mid = 25;
+var high = 50;
+var low = 1;
+var current = 13;
+var found = -1;
+if (current < mid) {
+    mid =(current-low)/2;
+}
