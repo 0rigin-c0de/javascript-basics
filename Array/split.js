7165440f530edc542ca89619442e243122326f3c
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-24 10:01:13 +0530

    initialize how split function works on simple string

---
+++ Array/split.js
@@ -0,0 +1,7 @@
+//Demonstrate how split function works on a simple string :
+
+  var sentence = "Do you even code bruh";
+  var words = sentence.split(" ");
+  for (var i = 0; i < words.length;++i){
+    console.log("word " + i + ": " + words[i]);
+  }
