0ee0a771cddc35881dc87cb770af6dbb0cfaf7fe
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 09:57:52 +0530

    initialize how to search value in array using indexOf(); method

---
+++ Array/AccessorFunctions/indexOf.js
@@ -0,0 +1,9 @@
+const cars =['BMW', 'Volvo', 'Lancer','Rubicon'];
+const car = cars.indexOf('Volvo');
+
+if(car > -1){
+    console.log('Yey, we have Volvo' );
+}
+else{
+    console.log('oh,  so sad')
+}
