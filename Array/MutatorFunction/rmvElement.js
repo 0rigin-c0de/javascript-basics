a529e4b04fbb2d934c5f9f6425ef1ae322042e06
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 21:20:47 +0530

    initialize how to remove a value from array in javascript

---
+++ Array/MutatorFunction/rmvElement.js
@@ -0,0 +1,13 @@
+let person =['Virat', 'Rohit','Rahul'];
+
+// remove the last element :-
+person.pop();
+console.log(person);
+
+
+let names = ['James', 'Nick', 'Johnson'];
+
+
+//remove the first element :- 
+names.shift();
+console.log();
