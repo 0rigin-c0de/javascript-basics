0b82a41cf977e4eadcd45dfc9578fe21df88ef51
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 21:52:58 +0530

    initialize how forEach(); function works on array

---
+++ Array/Iterators/Non-Array/forEach.js
@@ -0,0 +1,8 @@
+/*This function takes a function as an argument 
+ and applies the called function to each element of an array */
+
+function square(num) {
+    console.log(num, num * num);
+   }
+var nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12 ];
+nums.forEach(square);
