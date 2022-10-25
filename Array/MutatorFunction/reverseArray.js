3848bda09148974afbdccea06b79dd2f57771038
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 21:18:59 +0530

    how to reverse an array in Javascript

---
+++ Array/MutatorFunction/reverseArray.js
@@ -0,0 +1,8 @@
+/* reverse(); function reverses the order of the elements of an array. Here
+is an example of its use:  */
+
+var numbers = [23,24,25,26,27,28,29];
+numbers.reverse();
+console.log(numbers);
+
+
