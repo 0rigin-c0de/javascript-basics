b51d1617a4ccb28314f49734fe74258bcc18ed66
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 11:05:19 +0530

    initialize basic declaration of array

---
+++ Array/Array.js
@@ -0,0 +1,13 @@
+// create array using literal [ ];
+var arr1 = [1, 2, 3, 4, 5];
+
+// using new keyboard :-
+var arr2 = new Array(1, 3, 4, 5, 6, 7);
+
+// array with mixed data types :- 
+var arr3  =['work', 'exercise', 1 , true];
+
+var arr = ['hello',  23, 24];
+
+// nested array :-
+var nestedArr = [['hello', 23, 24] , ['World', 25, 26]];
