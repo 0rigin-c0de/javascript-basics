608cdc7fc1047188ce8fd22f8ff070d5da740998
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 11:25:10 +0530

    initialize how to add element at the start & at the end of  the array

---
+++ Array/addElement.js
@@ -0,0 +1,10 @@
+let petAnimals = ['cat', 'dog','cow','buffalo'];
+
+// add an element at the end :-
+    petAnimals.push('goat');
+    console.log(petAnimals);
+
+    // add an element at the start :-
+    petAnimals.unshift('hen');
+    console.log(petAnimals);
+    
