a4e34089b121dc1c4b41067fc631d2ca4358c748
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-25 21:21:20 +0530

    files are deleted

--- Array/OrderElements/reverseArray.js
+++
@@ -1,8 +0,0 @@
-/* reverse(); function reverses the order of the elements of an array. Here
-is an example of its use:  */
-
-var numbers = [23,24,25,26,27,28,29];
-numbers.reverse();
-console.log(numbers);
-
-
--- Array/OrderElements/reverseStringArray.js
+++
@@ -1,9 +0,0 @@
-/*Sort function works very well with strings */
-let  names = ['Brooklyn', 'Anthony', 'Steve','Elon', 'Virat', 'Ronaldo'];
-  names.sort();
-  console.log(names);
-
-/*But sort function do not work so well with numbers */
-  var nums = [3,1,2,100,4,200];
-nums.sort();
-console.log(nums); // 1,100,2,200,3,4
