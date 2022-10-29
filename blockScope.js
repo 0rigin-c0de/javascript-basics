7be673f18862cbd99b7848bacf3cd7b065360f83
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-29 14:42:05 +0530

    code deleted

--- blockScope.js
+++
@@ -1,9 +0,0 @@
-// The Scope variable inside the function is not declared with the var keyboard, when the string "local" is assigned to the variable, 
-//we are actually changing the value of the scope variable in the main program;
-
-
-// JavaScript does not have block scope, we pretend like it does when we write for loops in javaScript;
-for (var i = 1; i <= 10; ++i ) {
-    console.log("Hello, world!");
-   }
-   
