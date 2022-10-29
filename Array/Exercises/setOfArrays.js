5a641b8b4808e5f2c8c965e54554cfa03cd96fbe
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-10-29 10:23:02 +0530

    intialize array that displays both content forward and backward

---
+++ Array/Exercises/setOfArrays.js
@@ -0,0 +1,18 @@
+/*. Store a set of words in an array and display the contents both forward and backward. */
+var words = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(words);
+console.log(words.reverse());
+
+var words = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(words);
+console.log(words.reverse());
+
+ var words = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+words.reverse();
+console.log(words);
+
+var words = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(words);
+words.forEach(function(word) {
+console.log(word);
+});
