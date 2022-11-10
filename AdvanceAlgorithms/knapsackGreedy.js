501024d54cdabcae577b3b16551a86d49db2a47b
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-11-10 18:03:03 +0530

    solve knapsack problems using greedy algorithms

--- AdvanceAlgorithms/knapsackGreedy.js
+++ AdvanceAlgorithms/knapsackGreedy.js
@@ -1,11 +1,23 @@
-function ksack(values, weights, capacity) {
-  var load = 0;
+function knapsack(values, weights, capacity) {
+  var grab = 0;
   var i = 0;
   var w = 0;
-  while (load < capacity && i < 4) {
-    if (weights[i] <= capacity - load) {
+  while (grab < capacity && i < 4) {
+    if (weights[i] <= capacity - grab) {
       w += values[i];
-      load += weights[i];
+      grab += weights[i];
+    } else {
+      var r = (capacity - grab) / weights[i];
+      w += r * values[i];
+      grab += weights[i];
     }
+    ++i;
   }
+  return w;
 }
+
+var items = ["A", "B", "C", "D"];
+var values = [50, 140, 60, 60];
+var weights = [5, 20, 10, 12];
+var capacity = 30;
+console.log(knapsack(values, weights, capacity));
