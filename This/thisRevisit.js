724a83db43761ea89f95e4a61a50fad5847bb972
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-13 19:00:28 +0530

    objects jsHomework and mathHomework link to the single homework

---
+++ This/thisRevisit.js
@@ -0,0 +1,15 @@
+var homework = {
+  study() {
+    console.log(`Please study ${this.topic}`);
+  },
+};
+
+var jsHomework = Object.create(homework);
+jsHomework.topic = "JS";
+jsHomework.study();
+// Please study JS
+
+var mathHomework = Object.create(homework);
+mathHomework.topic = "Math";
+mathHomework.study();
+// Please study Math
